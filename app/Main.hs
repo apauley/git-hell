ea25e7886c0b08000570f35391460470e3267854
Andreas Pauley <andreas@pauley.org.za>
2016-06-29 11:37:58 +0200

    List git branches

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,9 @@ import Prelude hiding (FilePath)
 
 main :: IO ()
 main = do
+  view currentBranch
   echo "Hello git-hell"
+
+currentBranch :: Shell Text
+currentBranch = do
+  inproc "git" ["branch", "--list"] empty
