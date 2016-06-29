73d033e88ef2c3bb48ff7b18d294f7bf35c2d4f3
Andreas Pauley <andreas@pauley.org.za>
2016-06-29 13:36:33 +0200

    subcommands

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,21 @@ import Prelude hiding (FilePath)
 
 main :: IO ()
 main = do
-  stdout currentBranch
-  echo "Hello git-hell"
+  x <- options "git utilities" parser
+  case x of
+    CurrentBranch _ -> stdout currentBranch
+    _               -> echo "Not implemented"
+
+data Command = CurrentBranch (Maybe Text) | Status (Maybe Text) deriving (Show)
+
+parser :: Parser Command
+parser = fmap CurrentBranch (subcommand "current-branch" "Show the current git branch" noArgs)
+     <|> fmap Status        (subcommand "st" "git status" noArgs)
 
 currentBranch :: Shell Text
 currentBranch = do
   let branches = inproc "git" ["branch", "--list"] empty
   sed ("* " *> return "") $ grep (prefix "*") branches
+
+noArgs :: Parser (Maybe Text)
+noArgs = optional (argText "" "")
