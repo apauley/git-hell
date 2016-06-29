d7eb7d03d4690747afe8d4a6b8bd84d9f6453462
Andreas Pauley <andreas@pauley.org.za>
2016-06-29 16:23:58 +0200

    currentBranchOrNothing and currentBranchOrEmptyText

--- src/GitHellLib.hs
+++ src/GitHellLib.hs
@@ -1,12 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module GitHellLib
-    ( currentBranch
-    ) where
+module GitHellLib where
 
 import Turtle
+import qualified Control.Foldl as Fold
 
 currentBranch :: Shell Text
 currentBranch = do
   let branches = inproc "git" ["branch", "--list"] empty
   sed ("* " *> return "") $ grep (prefix "*") branches
+
+currentBranchOrNothing :: IO (Maybe Text)
+currentBranchOrNothing = fold currentBranch Fold.head
+
+currentBranchOrEmptyText :: IO Text
+currentBranchOrEmptyText = do
+  maybeBranch <- currentBranchOrNothing
+  let branch = case maybeBranch of
+        Just b  -> b
+        Nothing -> ""
+  return branch
