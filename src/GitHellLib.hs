dc4e19b7b71cbb6e2863656443acf240299e3da0
Andreas Pauley <andreas@jemstep.com>
2016-07-07 18:30:18 +0200

    Remove unused currentBranchOrEmptyText

--- src/GitHellLib.hs
+++ src/GitHellLib.hs
@@ -6,14 +6,6 @@ import Turtle
 import qualified Control.Foldl as Fold
 import HSHLib (emptyErrorText)
 
-currentBranchOrEmptyText :: IO Text
-currentBranchOrEmptyText = do
-  maybeBranch <- currentBranchOrNothing
-  let branch = case maybeBranch of
-        Just b  -> b
-        Nothing -> ""
-  return branch
-
 currentBranchOrNothing :: IO (Maybe Text)
 currentBranchOrNothing = fold currentBranch Fold.head
 
