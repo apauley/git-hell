e9b12c3e9ce1e145f10f1410afd432774f9a39fc
Andreas Pauley <andreas@pauley.org.za>
2016-07-21 18:03:29 +0200

    Parser for fetch-daemon

--- app/Main.hs
+++ app/Main.hs
@@ -11,13 +11,17 @@ main = do
   x <- options "git utilities" parser
   case x of
     CurrentBranch _ -> stdout currentBranch
-    _               -> echo "Not implemented"
+    FetchDaemon (path, sleepSeconds) -> echo "Not implemented"
 
-data Command = CurrentBranch (Maybe Text) | Status (Maybe Text) deriving (Show)
+data Command = CurrentBranch (Maybe Text) | FetchDaemon (FilePath, Maybe Int) deriving (Show)
 
 parser :: Parser Command
 parser = fmap CurrentBranch (subcommand "current-branch" "Show the current git branch" noArgs)
-     <|> fmap Status        (subcommand "st" "git status" noArgs)
+     <|> fmap FetchDaemon   (subcommand "fetch-daemon" "git status" fetchParser)
+
+fetchParser :: Parser (FilePath, Maybe Int)
+fetchParser = (,) <$> argPath "repo"  "The path to a git repository"
+                  <*> optional (argInt "sleepSeconds" "The number of seconds to sleep between fetches.")
 
 noArgs :: Parser (Maybe Text)
 noArgs = optional (argText "" "")
