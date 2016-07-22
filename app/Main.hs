81f2aeb8e0428ff6888b1bd5ce9a82a4767acfa8
Andreas Pauley <andreas@pauley.org.za>
2016-07-22 09:24:00 +0200

    CLI usage reflects that it can fetch multiple repos

--- app/Main.hs
+++ app/Main.hs
@@ -18,10 +18,10 @@ data Command = CurrentBranch (Maybe Text) | FetchDaemon (FilePath, Maybe Int) de
 
 parser :: Parser Command
 parser = fmap CurrentBranch (subcommand "current-branch" "Show the current git branch" noArgs)
-     <|> fmap FetchDaemon   (subcommand "fetch-daemon" "Runs a `git fetch` continuously for a given repository" fetchParser)
+     <|> fmap FetchDaemon   (subcommand "fetch-daemon" "Runs `git fetch` continuously for the given repositories" fetchParser)
 
 fetchParser :: Parser (FilePath, Maybe Int)
-fetchParser = (,) <$> argPath "repo"  "The path to a git repository"
+fetchParser = (,) <$> argPath "basedir"  "The path to a directory with git repositories, or the path to a single repo"
                   <*> optional (argInt "sleepSeconds" "The number of seconds to sleep between fetches.")
 
 noArgs :: Parser (Maybe Text)
