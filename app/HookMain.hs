930c8edb6e1bd4e0a5f4e91d609cd5d8d1c9ca13
Andreas Pauley <andreas@pauley.org.za>
2016-07-29 13:49:22 +0200

    Hook: print context and environment

--- app/HookMain.hs
+++ app/HookMain.hs
@@ -4,15 +4,34 @@ module Main where
 
 import Turtle
 import System.Environment (getProgName)
+import qualified Data.Text as T (pack)
 
 main :: IO ()
 main = do
-  me <- getProgName
+  me' <- getProgName
+  let me = T.pack me'
+  defaultMain me
   case me of
     "pre-push" -> do
       (remote, url) <- options "git pre-push hook" prePushParser
-      echo $ format ("Remote: "%s%" Url: "%s) remote url
-    name -> putStrLn name
+      prePushMain remote url
+    name -> echo $ format (s%" support not implemented") name
+
+defaultMain :: Text -> IO ()
+defaultMain me = do
+  echo $ format ("Script name: >"%s%"<") me
+  args <- arguments
+  echo $ format ("Args: >"%s%"<\n") $ repr args
+  dir <- pwd
+  echo $ format ("pwd: >"%fp%"<\n") dir
+  evars <- env
+  echo $ format ("Env: \n>"%s%"<\n") $ repr evars
+
+prePushMain :: Text -> Text -> IO ()
+prePushMain remote url = do
+  echo $ format ("Remote: "%s%" Url: "%s) remote url
+  input <- strict stdin
+  echo $ (format ("\n stdin: >"%s%"<") input)
 
 prePushParser :: Parser (Text, Text)
 prePushParser = (,) <$> argText "remote"  "Name of the remote to which the push is being done"
